a251feead236f046239ff92345be798fb3e5609e
Jessica-Maria Freund <jessicamariafreund@gmail.com>
2024-02-25 19:56:52 +0100

    Set global styles

--- client/src/themes/theme.tsx
+++ client/src/themes/theme.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 import { Raleway } from 'next/font/google';
-import { createTheme } from '@mui/material/styles';
+import { Theme, createTheme } from '@mui/material/styles';
 
 const raleway = Raleway({
   weight: ['400', '500', '700'],
@@ -14,21 +14,34 @@ const theme = createTheme({
     fontFamily: raleway.style.fontFamily,
   },
   palette: {
-    mode: 'light',
-    primary: {
-      main: '#9370DB',
-    },
-    secondary: {
-      main: '#f50057',
-    },
-    background: {
-      default: '#E6E6FA',
-      paper: '#F5F5F5',
-    },
     text: {
       primary: '#333333',
-      secondary: '#cccccc',
     },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.variant === 'contained' &&
+          {
+            backgroundColor: '#9581EB',
+            color: '#fff',
+            '&:hover': {
+              backgroundColor: '#7F6BC2',
+            },
+          }),
+          ...(ownerState.variant === 'text' && {
+            color: '#333333',
+            transition: 'color 0.3s ease',
+            '&:hover': {
+              backgroundColor: 'transparent',
+              color: '#9581EB',
+            },
+          }),
+        }),
+      },
+    },
+
   },
 });
 
