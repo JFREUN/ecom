64583982c84e0833e849b06e7a7355aa30ffc92a
Jessica-Maria Freund <jessicamariafreund@gmail.com>
2024-03-10 05:58:12 +0100

    Create authContext

---
+++ client/src/app/context/AuthContext.tsx
@@ -0,0 +1,107 @@
+'use client'
+import React, { useState, useEffect, createContext } from "react";
+import axios from "axios";
+
+const API_URL = process.env.API_URL;
+
+type User = {
+    id: string;
+    name: string;
+    email: string;
+};
+
+type AuthContextType = {
+    isLoggedIn: boolean;
+    isLoading: boolean;
+    user: User | null;
+    logOutUser: () => void;
+    storeToken: (token: string) => void;
+    authenticateUser: () => void;
+};
+
+const AuthContext = createContext<AuthContextType>({
+    isLoggedIn: false,
+    isLoading: false,
+    user: null,
+    logOutUser: () => { },
+    storeToken: (token: string) => { },
+    authenticateUser: () => { },
+});
+
+
+function AuthProvider({ children }: any) {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [user, setUser] = useState(null);
+
+    const storeToken = (token: string) => {
+        localStorage.setItem("authToken", token);
+    };
+
+    const authenticateUser = () => {
+        // Get the stored token from the localStorage
+        const storedToken = localStorage.getItem("authToken");
+
+        // If the token exists in the localStorage
+        if (storedToken) {
+            // We must send the JWT token in the request's "Authorization" Headers
+            axios
+                .get(`${API_URL}/auth/verify`, {
+                    headers: { Authorization: `Bearer ${storedToken}` },
+                })
+                .then((response) => {
+                    // If the server verifies that JWT token is valid  ✅
+                    const user = response.data;
+                    // Update state variables
+                    setIsLoggedIn(true);
+                    setIsLoading(false);
+                    setUser(user);
+                })
+                .catch((error) => {
+                    // If the server sends an error response (invalid token) ❌
+                    // Update state variables
+                    setIsLoggedIn(false);
+                    setIsLoading(false);
+                    setUser(null);
+                });
+        } else {
+            // If the token is not available
+            setIsLoggedIn(false);
+            setIsLoading(false);
+            setUser(null);
+        }
+    };
+
+    const removeToken = () => {
+        // Upon logout, remove the token from the localStorage
+        localStorage.removeItem("authToken");
+    };
+
+    const logOutUser = () => {
+        removeToken();
+        authenticateUser();
+    };
+
+    useEffect(() => {
+        // Run the function after the initial render,
+        // after the components in the App render for the first time.
+        authenticateUser();
+    }, []);
+
+    return (
+        <AuthContext.Provider
+            value={{
+                isLoggedIn,
+                isLoading,
+                user,
+                storeToken,
+                authenticateUser,
+                logOutUser,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export { AuthProvider, AuthContext };
--- client/src/app/context/GlobalProvider.tsx
+++ client/src/app/context/GlobalProvider.tsx
@@ -2,15 +2,18 @@ import { CartProvider } from "./CartContext"
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import theme from "../../themes/theme";
 import { ThemeProvider } from "@mui/material";
+import { AuthProvider } from "./AuthContext";
 
 export const GlobalProvider = ({ children }: any) => {
     return (
-        <CartProvider>
-            <AppRouterCacheProvider>
-                <ThemeProvider theme={theme}>
-                    {children}
-                </ThemeProvider>
-            </AppRouterCacheProvider>
-        </CartProvider>
+        <AuthProvider>
+            <CartProvider>
+                <AppRouterCacheProvider>
+                    <ThemeProvider theme={theme}>
+                        {children}
+                    </ThemeProvider>
+                </AppRouterCacheProvider>
+            </CartProvider>
+        </AuthProvider>
     )
 }
