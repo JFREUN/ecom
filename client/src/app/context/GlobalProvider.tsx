429e2d211952c7a46a259afef71285ba587c072b
Jessica-Maria Freund <jessicamariafreund@gmail.com>
2024-03-10 05:11:20 +0100

    Move theme and cacheProvider to global context provider

--- client/src/app/context/GlobalProvider.tsx
+++ client/src/app/context/GlobalProvider.tsx
@@ -1,9 +1,16 @@
 import { CartProvider } from "./CartContext"
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import theme from "../../themes/theme";
+import { ThemeProvider } from "@mui/material";
 
 export const GlobalProvider = ({ children }: any) => {
     return (
         <CartProvider>
-            {children}
+            <AppRouterCacheProvider>
+                <ThemeProvider theme={theme}>
+                    {children}
+                </ThemeProvider>
+            </AppRouterCacheProvider>
         </CartProvider>
     )
 }
--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import Navbar from "./components/Navbar"
-import theme from "../themes/theme";
-import { ThemeProvider } from "@mui/material";
 import Footer from "./components/Footer";
 import { GlobalProvider } from "./context/GlobalProvider";
 
@@ -23,13 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body >
         <GlobalProvider>
-          <AppRouterCacheProvider>
-            <ThemeProvider theme={theme}>
-              <Navbar />
-              {children}
-              <Footer />
-            </ThemeProvider>
-          </AppRouterCacheProvider>
+          <Navbar />
+          {children}
+          <Footer />
         </GlobalProvider>
       </body>
     </html>
