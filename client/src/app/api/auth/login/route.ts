82b30a165a8ca1cfa002ffd2de37d359b6b14796
Jessica-Maria Freund <jessicamariafreund@gmail.com>
2024-03-13 15:08:58 +0100

    Create login route

--- client/src/app/api/auth/login/route.ts
+++ client/src/app/api/auth/login/route.ts
@@ -1,12 +1,22 @@
-// import axios from "axios";
-// import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-// export async function POST(req: NextRequest) {
-//   try {
-//     const res = await axios.post(`${API_URL}/auth/signup`, requestBody);
-//     const data = await res.json();
-//     return Response.json(data);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const res = await fetch(`${process.env.API_URL}/auth/login`, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
